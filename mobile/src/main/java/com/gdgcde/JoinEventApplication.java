220c84aa7dbf94cdb50da4eedf27946c41477b40
Rodrigo Garcete <oneosmar89@gmail.com>
2017-09-19 22:45:56 -0400

    Add libraries versions

---
+++ mobile/src/main/java/com/gdgcde/JoinEventApplication.java
@@ -0,0 +1,11 @@
+package com.gdgcde;
+
+import android.app.Application;
+
+/**
+ * @author rorogarcete
+ * @version 0.0.1
+ * Copyright 2017 GDG CDE. All rights reserved
+ */
+public class JoinEventApplication extends Application {
+}
